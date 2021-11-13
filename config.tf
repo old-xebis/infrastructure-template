3c08c822ff1f9c6dcc7c7360b4fa6febaafdf1fe
Martin Bruzina <martin@bruzina.cz>
2021-11-13 12:55:55 +0100

    fix: terraform backend fails from gitlab ci

--- config.tf
+++ config.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "http" {
     # address, lock_address, unlock_address = CLI arguments, see README.md section Usage
-    username = "gitlab-ci"
+    username = "gitlab-ci-token"
     # password = environment variable TF_HTTP_PASSWORD
     lock_method   = "POST"
     unlock_method = "DELETE"
