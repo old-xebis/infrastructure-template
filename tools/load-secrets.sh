bbd4a3157fa1e7f41258ad28f02513501b24fb2e
Martin Bruzina <martin@bruzina.cz>
2021-11-26 19:18:15 +0100

    fix: load-secrets says it loaded only gitlab token

--- tools/load-secrets.sh
+++ tools/load-secrets.sh
@@ -12,7 +12,8 @@ export HCLOUD_TOKEN=""
 export TF_TARGET_ENV_NAME="Local"
 export TF_TARGET_ENV_SLUG="loc"
 
-echo "${BASH_SOURCE[0]} ✓ Secrets loaded: GL_TOKEN"
+echo "${BASH_SOURCE[0]} ✓ Secrets loaded: GL_TOKEN, TF_HTTP_PASSWORD (= GL_TOKEN), HCLOUD_TOKEN"
+echo "${BASH_SOURCE[0]} ✓ Variables set: TF_TARGET_ENV_NAME=$TF_TARGET_ENV_NAME, TF_TARGET_ENV_SLUG=$TF_TARGET_ENV_SLUG"
 
 # Set skip-worktree bit on this file to make sure the file wouldn't be commited or pushed, for more information see
 # <https://git-scm.com/docs/git-update-index#_skip_worktree_bit>
