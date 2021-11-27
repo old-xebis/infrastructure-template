106257e7d4d1c8e7fa6e8d96810ace257f3f2900
Martin Bruzina <martin@bruzina.cz>
2021-11-27 12:42:45 +0100

    feat: add hetzner test vm and mb, ci ssh keys

--- main.tf
+++ main.tf
@@ -1,10 +1,22 @@
 provider "hcloud" {
-  # Configuration options
 }
 
-data "hcloud_locations" "demo" {
+resource "hcloud_ssh_key" "ci" {
+  name       = "ci@gitlab.com"
+  public_key = file("${path.module}/keys/ci@gitlab.com.pub")
 }
 
-output "hcloud_locations_demo" {
-  value = data.hcloud_locations.demo.names
+resource "hcloud_ssh_key" "mb" {
+  name       = "mb@pc0"
+  public_key = file("${path.module}/keys/mb@pc0.pub")
+}
+
+resource "hcloud_server" "test" {
+  name        = "test"
+  server_type = "cx11"
+  image       = "ubuntu-20.04"
+  ssh_keys = [
+    hcloud_ssh_key.ci.id,
+    hcloud_ssh_key.mb.id
+  ]
 }
---
+++ output.tf
@@ -0,0 +1,3 @@
+output "hcloud_server_test_ipv4_address" {
+  value = hcloud_server.test.ipv4_address
+}
