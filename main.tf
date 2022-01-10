89aac350cd9b85a954a011fbbf360c24da353fac
Martin Bruzina <martin@bruzina.cz>
2022-01-10 19:07:31 +0100

    refactor: replace terraform variable prefix with env

--- main.tf
+++ main.tf
@@ -3,8 +3,12 @@ provider "hcloud" {
 
 resource "hcloud_server" "test" {
   count       = local.count
-  name        = "${local.prefix}test${count.index}"
+  name        = "${local.env}-test${count.index}"
   server_type = "cx11"
   image       = "ubuntu-20.04"
   user_data   = file("${path.module}/cloud-config.yml")
+
+  labels = {
+    "env" = "${local.env}"
+  }
 }
